5f1130f83beaadbbadbaf84177340acb0faeec32
acca90 <matheusrher@gmail.com>
2017-07-25 23:23:07 -0300

    primeiro de muitos

---
+++ cello/principal/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.template import loader, RequestContext
+from django.views import View
+
+
+class Principal(View):
+    template_name = 'template/index.html'
+
+    def get(self, request):
+        context = RequestContext(request)
+
+        return render(request, self.template_name)
---
+++ cello/static/js/principal/Principal.js
@@ -0,0 +1,18 @@
+/**
+ * Created by virgilio on 25/07/17.
+ */
+
+var Principal = (function ($) {
+
+    var principal = {};
+
+    var init = function () {
+        $('#helloworld').show();
+    };
+
+
+
+    principal.init = init;
+    return principal;
+
+})(jQuery);
